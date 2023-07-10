2265ab0dfacd575ad281628bf404e74679df26c8
Harshita <hpharshita99@gmail.com>
2023-07-10 13:16:34 +0530

    cards

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,54 +1,75 @@
 "use client"
 import React from "react";
-import classes from '../styles/Card.module.css';
+import { useEffect, useState } from "react";
+import classes from "../styles/Card.module.css";
 import ReactPlayer from "react-player";
+
+import Link from "next/link";
 
 const data = [
   {
     id: "1",
     videoLink: "https://youtu.be/ER9SspLe4Hg",
-    description: "Complete JavaScript tutorial",
+    description: "Complete JavaScript tutorial! Learn Basic to Advance level",
+    duration:"Duration:13:46",
   },
   {
     id: "2",
-    videoLink: "https://youtu.be/BsDoLVMnmZs",
-    description: "Complete HTML tutorial",
+    videoLink: "https://youtu.be/uX9OsJQ7FJY",
+    description: "Complete HTML tutorial! Learn Basic to Advance level",
+    duration:"Duration:8:27",
   },
   {
     id: "3",
-    videoLink: "https://youtu.be/Edsxf_NBFrw",
-    description: "Complete CSS tutorial",
+    videoLink: "https://youtu.be/EZ5q8sW3Glo",
+    description: "Complete CSS tutorial! Learn Basic to Advance level",
+    duration:"Duration:7:06"
   },
   {
     id: "4",
     videoLink: "https://youtu.be/tiLWCNFzThE",
-    description: "Complete React tutorial",
+    description: "Complete React tutorial! Learn Basic to Advance level",
+    duration:"Duration:13.30"
   },
   {
     id: "5",
     videoLink: "https://youtu.be/IIpiLZGTWuo",
-    description: "Complete Node tutorial",
+    description: "Complete Node tutorial! Learn Basic to Advance level",
+    duration:"Duration:18:20"
   },
 ];
 
 const Card = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  const handleVideoClick = (id) => {
+    
+  };
+
+
   return (
     <div className={classes.mainsection}>
       <div className={classes.container}>
         <div className={classes.cards}>
-          {data.map((card, i) => (
-            <div key={i} className={classes.card}>
-              {card.videoLink && (
-                <ReactPlayer
-                  url={card.videoLink}
-                  className={classes.player}
-                  controls
-                  width="100%"
-                  height="auto"
-                />
-              )}
-              <p>{card.description}</p>
-            </div>
+          {data.map((card) => (
+            <Link href={`/MainSection/${card.id}`} key={card.id}>
+              <div className={classes.card} onClick={() => handleVideoClick(card.id)}>
+                {isClient && card.videoLink && (
+                  <ReactPlayer
+                    url={card.videoLink}
+                    className={classes.player}
+                    controls
+                    width="100%"
+                    height="auto"
+                  />
+                )}
+                <p>{card.description}</p>
+                <h4>{card.duration}</h4>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
@@ -57,7 +78,6 @@ const Card = () => {
 };
 
 export default Card;
-
 
 
 
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,14 +1,31 @@
+"use client"
+import React, { useState } from "react";
 import classes from "../styles/Navbar.module.css";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log("Search query:", searchQuery);
+    
+  };
+
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbar_item}>
-        <form className={classes.searchform}>
+        <form className={classes.searchform} onSubmit={handleSearchSubmit}>
           <input
             className={classes.searchinput}
             type="text"
             placeholder="Search..."
+            value={searchQuery}
+            onChange={handleSearchInputChange}
           />
           <button className={classes.searchbutton} type="submit">
             Search
@@ -18,4 +35,5 @@ const Navbar = () => {
     </nav>
   );
 };
+
 export default Navbar;
